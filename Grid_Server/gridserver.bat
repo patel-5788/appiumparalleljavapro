cbb6e7d67e9c52b20358d22f04a55e1983f05014
Arvind <patel5788@gmail.com>
2016-05-05 21:27:02 +0530

    added

--- Grid_Server/gridserver.bat
+++ Grid_Server/gridserver.bat
@@ -1,1 +1,2 @@
-java -jar %~dp0selenium-server-standalone-2.43.0.jar -role hub http://127.0.0.1:4444/grid/console
+C:
+java -jar selenium-server-standalone-2.43.0.jar -role hub http://127.0.0.1:4444/grid/console
